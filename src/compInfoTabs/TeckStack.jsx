80715d584cd8573f6204ce3ab839acae8af99a30
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-09-07 16:09:57 +0200

    Added Techkstack categories and icons to Language

--- src/compInfoTabs/TeckStack.jsx
+++ src/compInfoTabs/TeckStack.jsx
@@ -5,8 +5,44 @@ const TechStack = () => {
     return (
         <>
         <Banner/>
-        <div> TechStack</div>
+        <div className='Layout'>
+
+            <div className='Content ContentTech'>
+            <div className='circle'>
+                <h2>Languages</h2> 
+                </div>
+                <ul className='Content__Languages'>                  
+                    <li><img src={require( "../Images/c-sharp.png")} alt= "C#"></img></li>
+                    <li><img src={require("../Images/javascript.png")} alt = "JS"></img></li>
+                    <li><img src={require("../Images/python.png")}></img></li>
+                    </ul>
+                    </div>
+                    <div className='Content'>
+                    <div className='circle'>
+                <h2>Frontend</h2> 
+                </div>
+                <ul className='Content__Frontend'></ul>
+                    
+                </div>
+                <div className='Content'>
+                <div className='circle'>
+                <h2>Backend</h2> 
+                </div>
+                    <ul className='Content__Backend'></ul>
+                        <li><image source= ""></image></li>
+                        
+                    </div>
+
+                    <div className='Content'>
+                    <div className='circle'>
+                <h2>Other</h2> 
+                </div>
+                    <ul className='Content__Other'></ul>
+                </div>
+        
+        </div>
         </>
+    
 
     );
 }

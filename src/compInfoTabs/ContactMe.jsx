826818e6e7335c077cf92ba30fddbe0b583c47e5
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-09-07 20:14:04 +0200

    Added Contact me buttons

--- src/compInfoTabs/ContactMe.jsx
+++ src/compInfoTabs/ContactMe.jsx
@@ -8,7 +8,12 @@ const ContactMe = () => {
     return (
         <>
         <Banner/>
-        <div> contact me</div>
+        <button onclick="window.location.href='https://github.com/MistyDoe';">
+      GitHub
+        </button>
+        <button onclick="window.location.href='https://docs.google.com/document/d/1L6FyANTqxvjJYX_iZmYmXdqS9i6oPJ0PaIaHmRqSiYo/edit?usp=sharing';">
+      Cv
+        </button>
         </>
 
     );

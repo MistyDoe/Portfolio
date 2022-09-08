3321408d34a9cc72f14a38b355a9d01fa9030748
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-09-08 10:54:00 +0200

    Added Contact page funtionality

--- src/compInfoTabs/ContactMe.jsx
+++ src/compInfoTabs/ContactMe.jsx
@@ -8,12 +8,23 @@ const ContactMe = () => {
     return (
         <>
         <Banner/>
-        <button onclick="window.location.href='https://github.com/MistyDoe';">
-      GitHub
-        </button>
-        <button onclick="window.location.href='https://docs.google.com/document/d/1L6FyANTqxvjJYX_iZmYmXdqS9i6oPJ0PaIaHmRqSiYo/edit?usp=sharing';">
-      Cv
-        </button>
+        <div className='Layout'>
+        <div className='Content'>
+        <div className='circle circle__Cont'>
+                <h2 className='Title__AboutMe'>Contact info</h2> 
+                </div>
+           <p className='Content__bttn'>
+            <div>
+                <a className='bttn' href="https://github.com/MistyDoe" target="_blank">GitHub</a><br/>
+                <a className='bttn' href="https://docs.google.com/document/d/1L6FyANTqxvjJYX_iZmYmXdqS9i6oPJ0PaIaHmRqSiYo/edit?usp=sharing'" target="_blank">Cv</a>
+            </div>
+           
+              <p className='Content__Email'> migle.urbonaite@appliedtechnology.se </p>           
+           </p>
+        </div>
+        </div>
+
+        
         </>
 
     );

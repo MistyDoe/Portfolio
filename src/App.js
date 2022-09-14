0fe10a18eccd459a0528eda3553ef414b65033a1
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-09-14 15:35:32 +0200

    deploy fix 8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
-import Banner from './compStatic/Banner.jsx'
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Banner from './compStatic/Banner.jsx';
+import AboutMe from './compInfoTabs/AboutMe';
+import TechStack from './compInfoTabs/TeckStack';
+import Projects from './compInfoTabs/Projects';
+import ContactMe from './compInfoTabs/ContactMe';
+
 
 function App() {
   return (
     <>
     <Banner/>
+    <Route  exact path = "/" component = {AboutMe}/>
+      <Route path = "/aboutme" element = {<AboutMe/>}/>
+      <Route path = "/teckstack" element = {<TechStack />}/>
+      <Route path = "/projects" element = {<Projects />}/>
+      <Route path = "/contactme" element = {<ContactMe />}/>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter, Route, Routes } from "react-router-dom";
+import App from './App';
 import AboutMe from './compInfoTabs/AboutMe';
 import TechStack from './compInfoTabs/TeckStack';
 import Projects from './compInfoTabs/Projects';
@@ -9,14 +10,8 @@ import ContactMe from './compInfoTabs/ContactMe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter basename='Portfolio'>
-    <Routes>
-      <Route path = "#/" element = {<AboutMe/>}/>
-      <Route path = "#/aboutme" element = {<AboutMe/>}/>
-      <Route path = "#/teckstack" element = {<TechStack />}/>
-      <Route path = "#/projects" element = {<Projects />}/>
-      <Route path = "#/contactme" element = {<ContactMe />}/>
-    </Routes>
+  <HashRouter >
+   <App/>   
   </HashRouter>
 
 );

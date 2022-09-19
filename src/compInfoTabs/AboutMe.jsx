00491ae5c4daa346f57f7ac53a2ecb5ef97102f8
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-09-19 14:29:01 +0200

    Update About me section

--- src/compInfoTabs/AboutMe.jsx
+++ src/compInfoTabs/AboutMe.jsx
@@ -13,12 +13,11 @@ const AboutMe = () => {
                 </div>
            <p className='Content__text'>           
 I am from Södertälje, where I live with my partner and two adorable kittens Ymir and Silver. <br/>
-I recently graduated from the full stack .Net boot camp offered by Salt. 
+Recently graduated from the full stack .Net boot-camp offered by Salt. 
 <br/>Before my career switch, I was planning a career in academia within the infectious disease modeling area. 
-I have studied infectious disease control, health informatics, bioinformatics, and public health. <br/>
-Through my studies, I developed a knowledge base and interest in mathematics and machine learning.
-Nowadays I am continuing to improve my skill set as a developer, in machine learning, and data sciences. <br/>  
-I have an interest in healthcare and finance domains.
+I have degres in  infectious disease control, health informatics, bioinformatics, and public health. <br/>
+Through my studies, I developed an interest in mathematics and machine learning.<br/>
+Nowadays I am continuously improving my skill set as a developer, in machine learning, and data sciences.
            </p>
         </div>
         </div>

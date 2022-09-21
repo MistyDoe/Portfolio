73a479b8bef995359aca684ef9995da652680f8d
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-09-21 16:51:21 +0200

    Mobile adaptation about me

--- src/compInfoTabs/AboutMe.jsx
+++ src/compInfoTabs/AboutMe.jsx
@@ -10,16 +10,21 @@ const AboutMe = () => {
         <div className='circle'>
                 <h2 className='Title__AboutMe'>About Me</h2> 
                 </div>
-           <p className='Content__text'>           
-I am from Södertälje, where I live with my partner and two adorable kittens Ymir and Silver. <br/>
-
+           <p className='Content__text'> 
+           HI! <br/>
+           My name is Migle and I am a Full stack .Net developer based in Sweden.         
+I am from Södertälje, where I live with my partner and two adorable kittens.
 Recently graduated from the full stack .Net boot-camp offered by Salt. 
-<br/>Before my career switch, I was planning a career in academia within the infectious disease modeling area. 
-I have degrees in  infectious disease control, health informatics, bioinformatics, and public health. <br/>
-Through my studies, I developed an interest in mathematics and machine learning.<br/>
+<br/>
+I have degrees in  infectious disease control, health informatics, bioinformatics, and public health.Through my studies, I developed an interest in mathematics and machine learning.<br/>
 Nowadays I am continuously improving my skill set as a developer, in machine learning, and data sciences.
-           </p>
+           </p><br/>
+        <div className='Proj__Bttn'>
+        <a className='bttn' href="https://github.com/MistyDoe" target="_blank">GitHub</a><br/>
+        <a className='bttn' href="https://docs.google.com/document/d/1L6FyANTqxvjJYX_iZmYmXdqS9i6oPJ0PaIaHmRqSiYo/edit?usp=sharing'" target="_blank">Cv</a>
+            </div>      
         </div>
+
         </div>
         </>
 

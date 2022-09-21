e748716c0185a346314ca61f9484c1ffe87a6f95
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-09-21 16:10:55 +0200

    Mobile adaptation Banner/NavBar/projects

--- src/compInfoTabs/Projects.jsx
+++ src/compInfoTabs/Projects.jsx
@@ -12,14 +12,12 @@ const Projects = () => {
                 <h2 className='Title__AboutMe'>Student portal</h2> 
                 </div>
            <div className='Project__Content'>
-            <div>
               <p className='Proj__text'>Graduation project.
-A student portal to provide access to all the resources. Authentication is done by using Auth0. The client was built using React and Material UI. For the backend Asp.net, web API were used, together with Entity framework core for database management.   </p>           
+A student portal to provide access to all the resources. Authentication is done by using Auth0. The client was built using React and Material UI. For the backend Asp.net framework was used, together with Entity framework core for database management.   </p>           
                 <div className='Proj__Bttn'>               
                 <a className='bttn' href="https://github.com/cetinarslan-it/Salt-Iverse" target="_blank">GitHub</a>
                 <a className='bttn' href="https://youtu.be/2B3mXdnSOKU" target="_blank">Demo</a>
-                </div>
-              </div>           
+                </div>                       
            </div>
         </div> 
         <div className='Content'>
